8463127bf497f630384b1d2481b7e4cc288a4155
woduf9173@naver.com <woduf9173@naver.com>
2022-03-05 19:39:11 +0900

    .

---
+++ database/migrations/2022_03_04_160040_create_free_board_likes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFreeBoardLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('free_board_likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("freeboard_id");
+            $table->foreignId("user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('free_board_likes');
+    }
+}
