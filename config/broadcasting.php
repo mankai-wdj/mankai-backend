5b4aff1c6e98d3911ec507b3319a15c724eb7ee1
tjdrb63 <tjdrb63@gmail.com>
2022-04-18 22:10:47 +0900

    test

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
-                'host' => 'http://api.mankai.shop/',
+                'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http'
             ],
