648723ce378bd9e917136575f32a5689c0eafdf3
jhpdevs <jhpdevs@g.yju.ac.kr>
2022-04-19 02:52:28 +0900

    test

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -42,10 +42,7 @@ return [
                 'port' => 6001,
                 'scheme' => 'https'
             ],
-            'curl_options' => [
-                CURLOPT_SSL_VERIFYHOST => 0,
-                CURLOPT_SSL_VERIFYPEER => 0,
-            ]
+
         ],
 
         'ably' => [
