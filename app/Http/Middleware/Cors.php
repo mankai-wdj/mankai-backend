2d9c4d135f455bcea14e328693300ba1b9d0e5fe
tjdrb63 <tjdrb63@gmail.com>
2022-04-19 00:00:42 +0900

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,9 +18,18 @@ class Cors
 
     public function handle(Request $request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: *');
+        $httpOrigin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : null;
+        if (in_array($httpOrigin, [
+            // 여기 Array에 허용하려는 IP 또는 DOMAIN을 추가하면 된다.
+            'http://localhost:3000', // Dev Client Server using CORS
+            'http://mankai.shop', // Prod Client Server using CORS
+        ]))
+        header("Access-Control-Allow-Origin: ${httpOrigin}");
         header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-        header('Access-Control-Allow-Credentials: false');
+        header('Access-Control-Allow-Credentials: true');
+        header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization,x-xsrf-token,authorization");
+        header("Content-type:text/html;charset=utf-8");
+
         return $next($request);
 
     }
