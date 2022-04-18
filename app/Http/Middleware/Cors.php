9ce9c0b9a6c7eee37f05bbbaab1676d626b1cb41
tjdrb63 <tjdrb63@gmail.com>
2022-04-18 23:21:48 +0900

    test

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,10 +18,10 @@ class Cors
 
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-        ->header('Access-Control-Allow-Origin: *')
-        ->header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS')
-        ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+        header('Access-Control-Allow-Credentials: false');
+        return $next($request);
 
     }
 }
