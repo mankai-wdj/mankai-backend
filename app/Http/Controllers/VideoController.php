917f453d6497028d3da61cdc41e1edb7fa639c77
chli-jiseong <chlwltjd98@naver.com>
2022-04-19 11:51:07 +0900

    commit

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -11,7 +11,7 @@ class VideoController extends Controller
     public function videoChatFileSave(Request $request) {
         if($request->hasFile("file")) {
             $fileName = time() . '_' . $request->file('file')->getClientOriginalName();
-            $path = $request->file('file')->storeAs('files/'.$request->room_id.'/'.date('Y-m-d').'/', $fileName, 's3');
+            $path = $request->file('file')->storeAs('files/'.$request->room_id.'/'.date('Y-m-d'), $fileName, 's3');
             $file_path = [];
               // return $request->file('file')->getSize();
             array_push($file_path, (object)array('path'=>Storage::url($path), 'size' =>$request->file('file')->getSize(), 'name' =>$request->file('file')->getClientOriginalName(), 'type'=>explode(".",$request->file('file')->getClientOriginalName())[count(explode(".",$request->file('file')->getClientOriginalName()))-1]));
