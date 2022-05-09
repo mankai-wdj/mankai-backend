5b78d658ba501394b1f2ddaee934fe69d33a6a9a
woduf9173@naver.com <woduf9173@naver.com>
2022-05-09 20:34:49 +0900

    BoardMemo상세보기

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -146,4 +146,10 @@ class MemoController extends Controller
         $images = MemoImage::where('memo_id', $memo_id)->get();
         return $images;
     }
+
+    public function showBoardMemo($memo_id)
+    {
+        $memo = AllMemo::find($memo_id);
+        return $memo;
+    }
 }
