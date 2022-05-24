a2f2539791b115dd84e97c3f03f434f1c19c2cdd
JHPDEVS <jhpdevs@g.yju.ac.kr>
2022-05-24 19:32:40 +0900

    -update pusher option

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -42,7 +42,7 @@ return [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
                 ]
-            ],`
+            ],
         ],
 
         'ably' => [
